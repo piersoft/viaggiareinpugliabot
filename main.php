f0e716088c93f5052c49808b20b155796afa684e
Francesco Piero Paolicelli <piersoft@me.com>
2016-01-23 11:09:53 +0100

    inlinebot

--- main.php
+++ main.php
@@ -29,6 +29,7 @@ function start($telegram,$update)
 {
 	date_default_timezone_set('Europe/Rome');
 	$today = date("Y-m-d H:i:s");
+	if (strpos($text,'@viaggiareinpugliabot') !== false) $text=str_replace("@viaggiareinpugliabot ","",$text);
 
 	if ($text == "/start" || $text == "Informazioni") {
 		$reply = "Benvenuto. Per ricercare un luogo di interesse turistico, culturale censito da ViaggiareinPuglia.it, digita il nome del Comune oppure clicca sulla graffetta (📎) e poi 'posizione' . Puoi anche ricercare per parola chiava nel titolo anteponendo il carattere ?. Verrà interrogato il DataBase openData utilizzabile con licenza IoDL2.0 presente su http://www.dataset.puglia.it/dataset/luoghi-di-interesse-turistico-culturale-naturalistico . In qualsiasi momento scrivendo /start ti ripeterò questo messaggio di benvenuto.\nQuesto bot, non ufficiale e non collegato con il marchio regionale ViaggiareinPuglia.it, è stato realizzato da @piersoft e potete migliorare il codice sorgente con licenza MIT che trovate su https://github.com/piersoft/viaggiareinpugliabot. La propria posizione viene ricercata grazie al geocoder di openStreetMap con Lic. odbl.";
@@ -65,6 +66,7 @@ function start($telegram,$update)
 			$img = curl_file_create('puglia.png','image/png');
 			$contentp = array('chat_id' => $chat_id, 'photo' => $img);
 			$telegram->sendPhoto($contentp);
+
 			if(strpos($text,'?') !== false){
 				$text=str_replace("?","",$text);
 				$location="Sto cercando i luoghi aventi nel titolo: ".$text;
@@ -94,19 +96,20 @@ function start($telegram,$update)
 						$content = array('chat_id' => $chat_id, 'text' => $location,'disable_web_page_preview'=>true);
 						$telegram->sendMessage($content);
 			}
-			function decode_entities($text)
+			function decode_entities($textt)
 			{
 
-							$text=htmlentities($text, ENT_COMPAT,'ISO-8859-1', true);
-						$text= preg_replace('/&#(\d+);/me',"chr(\\1)",$text); #decimal notation
-							$text= preg_replace('/&#x([a-f0-9]+);/mei',"chr(0x\\1)",$text);  #hex notation
-						$text= html_entity_decode($text,ENT_COMPAT,"UTF-8"); #NOTE: UTF-8 does not work!
+							$textt=htmlentities($textt, ENT_COMPAT,'ISO-8859-1', true);
+						$textt= preg_replace('/&#(\d+);/me',"chr(\\1)",$textt); #decimal notation
+							$textt= preg_replace('/&#x([a-f0-9]+);/mei',"chr(0x\\1)",$textt);  #hex notation
+						$textt= html_entity_decode($textt,ENT_COMPAT,"UTF-8"); #NOTE: UTF-8 does not work!
 
-							return $text;
+							return $textt;
 			}
 
 			$result=0;
-
+			$ciclo=0;
+//if ($count > 40) $count=40;
   for ($i=$inizio;$i<$count;$i++){
 
 if ($string==1) {
@@ -115,7 +118,10 @@ if ($string==1) {
 	$filter=strtoupper($csv[$i][3]);
 }
 
+
 if (strpos(decode_entities($filter),strtoupper($text)) !== false ){
+				$ciclo++;
+//	if ($ciclo >40) exit;
 
 				$result=1;
 				$homepage .="\n";
@@ -142,6 +148,13 @@ if (strpos(decode_entities($filter),strtoupper($text)) !== false ){
 
 				$homepage .="\n____________\n";
 				}
+				if ($ciclo >40) {
+					$location="Troppi risultati per essere visualizzati. Restringi la ricerca";
+					$content = array('chat_id' => $chat_id, 'text' => $location,'disable_web_page_preview'=>true);
+					$telegram->sendMessage($content);
+
+					 exit;
+				}
 				}
 
 		$chunks = str_split($homepage, self::MAX_LENGTH);
@@ -152,9 +165,9 @@ if (strpos(decode_entities($filter),strtoupper($text)) !== false ){
 		}
 
 
-		$this->create_keyboard_temp($telegram,$chat_id);
 
-}
+	}
+	$this->create_keyboard_temp($telegram,$chat_id);
 
 	}
 
@@ -173,6 +186,7 @@ function location_manager($telegram,$user_id,$chat_id,$location)
 
 			$lon=$location["longitude"];
 			$lat=$location["latitude"];
+			$r=1;
 			$response=$telegram->getData();
 			$response=str_replace(" ","%20",$response);
 
@@ -189,7 +203,7 @@ function location_manager($telegram,$user_id,$chat_id,$location)
 				}else 	$comune .=$parsed_json->{'address'}->{'city'};
 
 				if ($parsed_json->{'address'}->{'village'}) $comune .=$parsed_json->{'address'}->{'village'};
-				$location="Sto cercando le località contenti \"".$comune."\" tramite le coordinate che hai inviato: ".$lat.",".$lon;
+				$location="Sto cercando le località contenenti \"".$comune."\" tramite le coordinate che hai inviato: ".$lat.",".$lon;
 				$content = array('chat_id' => $chat_id, 'text' => $location,'disable_web_page_preview'=>true);
 				$telegram->sendMessage($content);
 
@@ -223,11 +237,36 @@ function location_manager($telegram,$user_id,$chat_id,$location)
 
 			$result=0;
 
+			$ciclo=0;
+//if ($count >40) $count=40;
 	for ($i=$inizio;$i<$count;$i++){
 
-	$filter=strtoupper($csv[$i][3]);
+		$lat10=floatval($csv[$i][7]);
+		$long10=floatval($csv[$i][8]);
+		$theta = floatval($lon)-floatval($long10);
+		$dist =floatval( sin(deg2rad($lat)) * sin(deg2rad($lat10)) +  cos(deg2rad($lat)) * cos(deg2rad($lat10)) * cos(deg2rad($theta)));
+		$dist = floatval(acos($dist));
+		$dist = floatval(rad2deg($dist));
+		$miles = floatval($dist * 60 * 1.1515 * 1.609344);
+	//echo $miles;
+
+		if ($miles >=1){
+			$data1 =number_format($miles, 2, '.', '');
+			$data =number_format($miles, 2, '.', '')." Km";
+		} else {
+			$data =number_format(($miles*1000), 0, '.', '')." mt";
+			$data1 =number_format(($miles*1000), 0, '.', '');
+		}
+		$csv[$i][100]= array("distance" => "value");
+
+		$csv[$i][100]= $data;
+
+
+
+		$filter=strtoupper($csv[$i][3]);
 
 if (strpos(decode_entities($filter),strtoupper($comune)) !== false ){
+	$ciclo++;
 
 				$result=1;
 				$homepage .="\n";
@@ -248,12 +287,20 @@ if (strpos(decode_entities($filter),strtoupper($comune)) !== false ){
 				if($csv[$i][18] !=NULL)	$homepage .="Foto2: ".decode_entities($csv[$i][18])."\n";
 				if($csv[$i][19] !=NULL) $homepage .="(realizzata da: ".decode_entities($csv[$i][19]).")\n";
 				if($csv[$i][7] !=NULL){
+					$homepage .="Dista: ".$csv[$i][100]."\n";
 					$homepage .="Mappa:\n";
 					$homepage .= "http://www.openstreetmap.org/?mlat=".$csv[$i][7]."&mlon=".$csv[$i][8]."#map=19/".$csv[$i][7]."/".$csv[$i][8];
 				}
 
 				$homepage .="\n____________\n";
 				}
+					if ($ciclo >40) {
+						$location="Troppi risultati per essere visualizzati. Restringi la ricerca";
+						$content = array('chat_id' => $chat_id, 'text' => $location,'disable_web_page_preview'=>true);
+						$telegram->sendMessage($content);
+
+						 exit;
+					}
 				}
 
 				$chunks = str_split($homepage, self::MAX_LENGTH);
